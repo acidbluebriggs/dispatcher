aa0054a3f4acd71b425c0a060523439f5e3ebb6f
Briggs <briggs@acidblue.com>
2024-10-18 10:55:04 -0400

    Syntax update.

--- invariant.js
+++ invariant.js
@@ -21,7 +21,8 @@
  * The invariant message will be stripped in production, but the invariant
  * will remain to ensure logic does not differ in production.
  */
-var __DEV__ = true;
+const __DEV__ = true;
+
 export function invariant(condition, format, a, b, c, d, e, f) {
   if (__DEV__) {
     if (format === undefined) {
@@ -30,22 +31,24 @@ export function invariant(condition, format, a, b, c, d, e, f) {
   }
 
   if (!condition) {
-    var error;
+    let error;
     if (format === undefined) {
       error = new Error(
         'Minified exception occurred; use the non-minified dev environment ' +
         'for the full error message and additional helpful warnings.'
       );
     } else {
-      var args = [a, b, c, d, e, f];
-      var argIndex = 0;
+      const args = [a, b, c, d, e, f];
+      let argIndex = 0;
       error = new Error(
         'Invariant Violation: ' +
-        format.replace(/%s/g, function() { return args[argIndex++]; })
+        format.replace(/%s/g, function () {
+          return args[argIndex++];
+        })
       );
     }
 
     error.framesToPop = 1; // we don't care about invariant's own frame
     throw error;
   }
-};
+}
